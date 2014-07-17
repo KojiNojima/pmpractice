fcecbf97d2f0367169b14b23b2f1119f55604bad
KojiNojima <kou520kou520@gmial.com>
2014-07-17 13:58:02 +0900

    takuma-b
    test

---
+++ takuma-b/database_conf.php
@@ -0,0 +1,16 @@
+<?php
+// データベース設定
+$dbServer = '127.0.0.1';
+$dbUser   = 'A';
+$dbPass   = '0';
+$dbName   = 'menu';
+
+# MySQL用のDSN文字列です。
+$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
---
+++ takuma-b/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<html>
+    <head>
+        <title>学食メニュー推奨システム</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style type="text/css">
+            #top {
+                position: fixed;
+            }
+            #menu {
+                padding-top: 150px;
+            }
+            #menu table {
+                position: fixed;  
+            }
+            #menu td {
+                width: 110px;
+            }
+            #main {
+                padding-left: 180px;
+            }
+        </style>
+    </head>
+	<body>
+            <div id="top">
+            <img src="image/title.png" alt="プロジェクトマネジメント学科" width="900" height="100">
+            </div>
+            <div id="menu">
+                <form method="post" action="search.php">
+            <table>
+                <tr>
+			<td height="30" align="center">
+			  体重
+			</td>
+			<td>
+			  <input name="tai" value="" size="6" style="text-align:right" maxlength="3">kg
+			</td>
+		</tr>
+		<tr>
+			<td height="30" align="center">
+			  身長
+			</td>
+			<td>
+			  <input name="sin" value="" size="6" style="text-align:right" maxlength="3">cm
+			</td>
+		</tr>
+		<tr>
+			<td height="40" align="center" colspan="2">
+                            <input type="submit" value="計算！">
+			</td>
+		</tr>
+		<tr>
+			<td height="30" align="center">
+			  BMI指数
+			</td>
+			<td width="40"  style="text-align:right">
+			</td>
+		</tr>
+		<tr>
+			<td height="30" align="center">
+			  適正カロリー
+			</td>
+			<td width="40"  style="text-align:right">
+                            kcl
+			</td>
+		</tr>
+		<tr>
+			<td height="30" align="center">
+			  適正体重
+			</td>
+			<td width="40"  style="text-align:right">
+                            kg
+			</td>
+		</tr>
+		<tr>
+			<td align="center" colspan="2">
+			    <img src="image/message.png" alt="メッセージ" width="80%" name="message">
+			    <span style="position: absolute; top: 255px; left: 40px; width: 130px;">
+			    <div id="text">身長と体重を入力してください</div>
+			    </span>
+			</td>
+		</tr>		<tr>
+			<td align="center" colspan="2">
+			    <img src="image/face_smile.png" alt="顔" width="80%" name="face">
+			</td>
+		</tr>
+	  </table>
+                 
+	</form>
+           
+	</div>
+	<div id="main">
+    <div style="text-align:center">
+    <h1>ランチメニュー</h1>
+    <img src="image/menu.PNG" alt="メニュー" width="80%" >
+    </div>
+    <div style="text-align:right">
+    </div>
+    </div>
+    </body>
+</html>
