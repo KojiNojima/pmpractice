58f445f8de6ec8b9b94fff11a8f3fcff9e60e310
KojiNojima <kou520kou520@gmial.com>
2014-07-25 09:23:39 +0900

    takuma-c
    納品いたします。

--- takuma-b/takuma-b/database_conf.php
+++
@@ -1,9 +0,0 @@
-<?php
-// データベース設定
-$dbServer = '175.184.33.52';
-$dbUser   = 'ccwoeok_agw';
-$dbPass   = 'VdR1UoAC';
-$dbName   = 'ccwoeok_agw';
-
-# MySQL用のDSN文字列です。
-$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
